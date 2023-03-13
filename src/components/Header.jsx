eecd05a966ea4aa3c1b73bb67d1cefc3becea71b
smerch88 <maxarmyk@gmail.com>
2023-03-13 21:40:22 +0200

    feat: add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,85 @@
+import {
+  AppBar,
+  Container,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+} from '@mui/material';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import FindInPageOutlinedIcon from '@mui/icons-material/FindInPageOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleGithubiconClick = () => {
+    window.open('https://github.com/smerch88', '_blank');
+  };
+
+  const menu = (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton component={Link} to="/">
+          <HomeOutlinedIcon sx={{ marginRight: '40px' }} />
+          <ListItemText primary="TNN" sx={{ marginRight: '40px' }} />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component={Link} to="/departments">
+          <FindInPageOutlinedIcon sx={{ marginRight: '40px' }} />
+          <ListItemText primary="Departments" sx={{ marginRight: '40px' }} />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+
+  return (
+    <>
+      <AppBar position="static">
+        <Container>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, marginLeft: '40px' }}
+            >
+              TTN Nova Poshta
+            </Typography>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleGithubiconClick}
+            >
+              <GitHubIcon />
+            </IconButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer anchor="top" open={isMenuOpen} onClose={toggleMenu}>
+        {menu}
+      </Drawer>
+    </>
+  );
+};
