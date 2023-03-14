23871ad9c59bbb64196ab30d2fd1fec264034100
smerch88 <maxarmyk@gmail.com>
2023-03-15 00:43:14 +0200

    update: more visual data to card of TTN

--- src/components/TTN/TnnList.jsx
+++ src/components/TTN/TnnList.jsx
@@ -1,14 +1,21 @@
 import {
+  Box,
   Button,
   Card,
   CardContent,
   Divider,
   Grid,
+  Icon,
   List,
   ListItem,
   Skeleton,
   Typography,
+  useTheme,
 } from '@mui/material';
+import MarkunreadMailboxIcon from '@mui/icons-material/MarkunreadMailbox';
+import SendTimeExtensionIcon from '@mui/icons-material/SendTimeExtension';
+import AllInboxIcon from '@mui/icons-material/AllInbox';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import {
@@ -22,12 +29,16 @@ import {
   deleteQueryTnn,
   setQueryTnn,
 } from 'redux/tnn/tnn-slice';
+
 import SwipeableEdgeDrawer from './SwipeableEdgeDrawer';
+
 import { TtnSavedCards } from './TtnSavedCards';
 
 export const TnnList = () => {
   const tnnData = useSelector(getTnn);
   const tnnListData = useSelector(getTnnList);
+
+  const theme = useTheme();
 
   const error = useSelector(getError);
   const isLoading = useSelector(getIsLoading);
@@ -63,27 +74,121 @@ export const TnnList = () => {
             >
               <Card sx={{ width: '100%' }}>
                 <CardContent>
-                  {isLoading ? (
-                    <Skeleton />
-                  ) : (
-                    <Typography>Статус доставки: {document.Status}</Typography>
-                  )}
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Icon
+                      sx={{
+                        marginRight: theme.spacing(4),
+                        marginLeft: theme.spacing(2),
+                      }}
+                    >
+                      <MarkunreadMailboxIcon />
+                    </Icon>
+                    <Box
+                      sx={{
+                        width: '100%',
+                      }}
+                    >
+                      {isLoading ? (
+                        <Skeleton />
+                      ) : (
+                        <Typography>
+                          Статус доставки:{' '}
+                          {document.Status.length
+                            ? document.Status
+                            : 'дані відсутні...'}
+                        </Typography>
+                      )}
+                      {isLoading ? (
+                        <Skeleton />
+                      ) : (
+                        <Typography variant="body2" color="grey" mb={1}>
+                          Дата доставки:{' '}
+                          {document.TrackingUpdateDate.length
+                            ? document.TrackingUpdateDate
+                            : 'дані відсутні...'}
+                        </Typography>
+                      )}
+                    </Box>
+                  </Box>
                   <Divider />
-                  {isLoading ? (
-                    <Skeleton />
-                  ) : (
-                    <Typography>
-                      Відправлено: {document.WarehouseSender}
-                    </Typography>
-                  )}
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Icon
+                      sx={{
+                        marginRight: theme.spacing(4),
+                        marginLeft: theme.spacing(2),
+                      }}
+                    >
+                      <SendTimeExtensionIcon />
+                    </Icon>
+                    <Box
+                      sx={{
+                        width: '100%',
+                      }}
+                    >
+                      {isLoading ? (
+                        <Skeleton />
+                      ) : (
+                        <Typography mt={1}>
+                          Відправлено:{' '}
+                          {document.WarehouseSender.length
+                            ? document.WarehouseSender
+                            : 'дані відсутні...'}
+                        </Typography>
+                      )}
+                      {isLoading ? (
+                        <Skeleton />
+                      ) : (
+                        <Typography variant="body2" color="grey" mb={1}>
+                          Дата створення:{' '}
+                          {document.DateCreated.length
+                            ? document.DateCreated
+                            : 'дані відсутні...'}
+                        </Typography>
+                      )}{' '}
+                    </Box>
+                  </Box>
                   <Divider />
-                  {isLoading ? (
-                    <Skeleton />
-                  ) : (
-                    <Typography>
-                      Отримано: {document.WarehouseRecipient}
-                    </Typography>
-                  )}
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Icon
+                      sx={{
+                        marginRight: theme.spacing(4),
+                        marginLeft: theme.spacing(2),
+                      }}
+                    >
+                      <AllInboxIcon />
+                    </Icon>
+                    <Box
+                      sx={{
+                        width: '100%',
+                      }}
+                    >
+                      {isLoading ? (
+                        <Skeleton />
+                      ) : (
+                        <Typography mt={1}>
+                          Отримано:{' '}
+                          {document.WarehouseRecipient.length
+                            ? document.WarehouseRecipient
+                            : 'дані відсутні...'}
+                        </Typography>
+                      )}
+                      {isLoading ? (
+                        <Skeleton />
+                      ) : (
+                        <Typography variant="body2" color="grey">
+                          Дата прибуття:{' '}
+                          {document.ActualDeliveryDate.length
+                            ? document.ActualDeliveryDate
+                            : 'дані відсутні...'}
+                        </Typography>
+                      )}{' '}
+                    </Box>
+                  </Box>
                   <Divider />
                 </CardContent>
               </Card>
