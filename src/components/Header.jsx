85736d91116dcb3cec29f4a63554d3cced399d7f
smerch88 <maxarmyk@gmail.com>
2023-03-14 20:45:10 +0200

    update: adaptive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,25 @@ import {
   ListItemButton,
   ListItemText,
   Toolbar,
+  Box,
+  Button,
 } from '@mui/material';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import FindInPageOutlinedIcon from '@mui/icons-material/FindInPageOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const pages = [
+    { id: '1', title: 'Пошук за ТТН', link: '/' },
+    { id: '2', title: 'Пошук відділень', link: '/departments' },
+  ];
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -54,7 +62,11 @@ export const Header = () => {
       <AppBar position="static">
         <Container>
           <Toolbar>
+            <LocalShippingIcon
+              sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
+            />
             <IconButton
+              sx={{ display: { md: 'none' } }}
               edge="start"
               color="inherit"
               aria-label="menu"
@@ -64,11 +76,33 @@ export const Header = () => {
             </IconButton>
             <Typography
               variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, marginLeft: '40px' }}
+              noWrap
+              component={Link}
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
             >
-              Nova Poshta Helper
+              Nova
             </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map(({ id, title, link }) => (
+                <Button
+                  key={id}
+                  component={Link}
+                  to={link}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {title}
+                </Button>
+              ))}
+            </Box>
             <IconButton
               edge="start"
               color="inherit"
